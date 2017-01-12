anonymousjerry/react-native

commit 28f1c67ced5f082d92ba674e7e8bd233f4ff29ca

packager: centralize babelRegisterOnly callsites
Reviewed By: davidaurelio

Differential Revision: D4380987

fbshipit-source-id: db34e56b2025fb072798ef2cc3e52f3d1cb7c33c

setupBabel.js
@@ -1,0 +1,47 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+'use strict';
+
+const babelRegisterOnly = require('./packager/babelRegisterOnly');
+const escapeRegExp = require('lodash/escapeRegExp');
+const path = require('path');
+
+const BABEL_ENABLED_PATHS = [
+  'packager/react-packager/src',
+  'local-cli',
+];
+
+/**
+ * We use absolute paths for matching only the top-level folders reliably. For
+ * example, we would not want to match some deeply nested forder that happens to
+ * have the same name as one of `BABEL_ENABLED_PATHS`.
+ */
+function buildRegExps(basePath, dirPaths) {
+  return dirPaths.map(folderPath =>
+    new RegExp(`^${escapeRegExp(path.resolve(basePath, folderPath))}`)
+  );
+}
+
+function getOnlyList() {
+  return buildRegExps(__dirname, BABEL_ENABLED_PATHS);
+}
+
+/**
+ * Centralized place to register all the directories that need a Babel
+ * transformation before being fed to Node.js. Notably, this is necessary to
+ * support Flow type annotations.
+ */
+function setupBabel() {
+  babelRegisterOnly(getOnlyList());
+}
+
+setupBabel.buildRegExps = buildRegExps;
+setupBabel.getOnlyList = getOnlyList;
+module.exports = setupBabel;

cli.js
@@ -8,8 +8,4 @@
  */
 'use strict';
 
-require('./packager/babelRegisterOnly')([
-  /private-cli\/src/
-]);
-
 module.exports = require('./local-cli/cli.js');

local-cli/cli.js
@@ -15,11 +15,7 @@ require('graceful-fs').gracefulify(require('fs'));
 // it is not supported. This is why the rest of the cli code is in `cliEntry.js`.
 require('./server/checkNodeVersion')();
 
-require('../packager/babelRegisterOnly')([
-  /private-cli\/src/,
-  /local-cli/,
-  /react-packager\/src/,
-]);
+require('../setupBabel')();
 
 var cliEntry = require('./cliEntry');
 

packager/react-packager/index.js
@@ -8,7 +8,7 @@
  */
 'use strict';
 
-require('../babelRegisterOnly')([/react-packager\/src/]);
+require('../../setupBabel')();
 
 const debug = require('debug');
 const Logger = require('./src/Logger');

packager/react-packager/src/JSTransformer/worker/index.js
@@ -9,8 +9,5 @@
 
 'use strict';
 
-const path = require('path');
-require('../../../../babelRegisterOnly')([
-  path.resolve(path.join(__dirname, '../../**/*')),
-]);
+require('../../../../../setupBabel')();
 module.exports = require('./worker');

commit 05853e05d647b381fbd1b155a64ec95a63e4e78d

Fix babel only regexp on windows
Summary:
cli / packager was broken on Windows after 28f1c67ced5f082d92ba674e7e8bd233f4ff29ca.

Babel `only` regexp option requires paths to use forward slashes only but `__dirname` will use backslashes on windows. This simply adds a replace to normalize to forward slashes.

cc jeanlauliac cpojer
Closes https://github.com/facebook/react-native/pull/11850

Reviewed By: cpojer

Differential Revision: D4409143

Pulled By: jeanlauliac

fbshipit-source-id: a705236630959f762e53100299ab073ec9a29ee1

setupBabel.js
@@ -26,7 +26,9 @@ const BABEL_ENABLED_PATHS = [
  */
 function buildRegExps(basePath, dirPaths) {
   return dirPaths.map(folderPath =>
-    new RegExp(`^${escapeRegExp(path.resolve(basePath, folderPath))}`)
+    // Babel `only` option works with forward slashes in the RegExp so replace
+    // backslashes for Windows.
+    new RegExp(`^${escapeRegExp(path.resolve(basePath, folderPath).replace(/\\/g, '/'))}`)
   );
 }
 